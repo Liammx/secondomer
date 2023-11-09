26aa45c803273b743664b456755bcd518d247178
liammx <liammx03@gmial.com>
2023-11-09 16:30:00 +0500

    nothing to say

---
+++ main.js
@@ -0,0 +1,33 @@
+let start = document.querySelector('.button1')
+let stop = document.querySelector('.button2')
+let concern = document.querySelector('.button3')
+let minute = document.querySelector('.minute')
+let second = document.querySelector('.second')
+
+let interval;
+let s = 0
+let m = 0
+
+start.addEventListener('click' , () =>{
+    interval = setInterval(() =>{
+        s++
+        if(s <= 9) {
+            second.innerHTML = `0${s}`
+        } else{
+            second.innerHTML = s
+        }
+
+        if(s == 60) {
+            m++
+            minute.innerHTML = m
+            s = 0 
+            second.innerHTML = s
+        }
+    }, 100)
+} )
+
+stop.addEventListener('click', () =>{
+    clearInterval(interval)
+})
+concern.addEventListener('click', () =>{
+})
